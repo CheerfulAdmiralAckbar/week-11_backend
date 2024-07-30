8ad3b970c002cb91cd9d7ac121829abfafcb7763
LastGreenseer <170721131+LastGreenseer@users.noreply.github.com>
2024-07-30 15:33:31 +0100

    fix:syntax error

--- src/characters/routes.js
+++ src/characters/routes.js
@@ -5,6 +5,6 @@ const { addCharacter, deleteCharacter } = require("./controllers");
 
 charRouter.post("/addCharacter", addCharacter);
 
-charRouter.delete("/Deletecharacter/:name", deleteCharacter)
+charRouter.delete("/deletecharacter/:name", deleteCharacter)
 
 module.exports = charRouter;
