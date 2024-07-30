9f28a3014949e9a9c2c42233eeec327f28dd085c
Rowan12628 <170511926+Rowan12628@users.noreply.github.com>
2024-07-30 14:17:06 +0100

    fix: make login work with signup

--- src/users/controllers.js
+++ src/users/controllers.js
@@ -1,9 +1,44 @@
 const User = require("./model");
 
-const login = async (req, res) => {
+const createUser = async (req, res) => {
+  console.log("Request Body: ", req.body);
   try {
-    if (!req.user) {
+    const { username, email, password, confirmPassword } = req.body;
+
+    if (password !== confirmPassword) {
+      return res.status(401).json({ message: "Passwords do not match" });
+    }
+
+    const newUser = await User.create({
+      username: username,
+      email: email,
+      password: password,
+    });
+
+    res
+      .status(201)
+      .json({ message: "User successfully created", user: newUser });
+  } catch (error) {
+    res
+      .status(500)
+      .json({ message: "Error creating user", error: error.message });
+  }
+};
+
+const login = async (req, res) => {
+  const { username, password } = req.body;
+  try {
+    const user = await User.scope("withPassword").findOne({
+      where: { username },
+    });
+
+    if (!user) {
       res.status(404).json({ message: "user not found" });
+    }
+    const isMatch = await user.isMatch(password);
+
+    if (!isMatch) {
+      return res.status(400).json({ message: "Password is incorrect" });
     }
     res.status(201).json({ message: "success" });
   } catch (error) {
@@ -11,4 +46,4 @@ const login = async (req, res) => {
   }
 };
 
-module.exports = { login: login };
+module.exports = { createUser, login: login };
--- src/users/routes.js
+++ src/users/routes.js
@@ -1,9 +1,10 @@
 const { Router } = require("express");
 const userRouter = Router();
 
-const { login } = require("./controllers");
+const { createUser, login } = require("./controllers");
 const {} = require("../middleware/auth");
 
+userRouter.post("/signup", createUser);
 userRouter.post("/login", login);
 
 module.exports = userRouter;
