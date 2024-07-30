6f32f370a505015695c625e66fb21a8c25cfe11c
CheerfulAdmiralAckbar <admiralackbar411@gmail.com>
2024-07-30 12:27:48 +0100

    feature: signup route and controller to create user account

--- src/users/controllers.js
+++ src/users/controllers.js
@@ -1,3 +1,26 @@
 const User = require("./model");
 
-module.exports = {};
+const createUser = async (req, res) => {
+  console.log('Request Body: ', req.body);
+  try {
+    const { username, email, password, confirmPassword } = req.body;
+
+    if (password !== confirmPassword) {
+      return res.status(401).json({ message: 'Passwords do not match' });
+    }
+
+    const newUser = await User.create({
+      username: username,
+      email: email,
+      password: password
+    });
+
+    res.status(201).json({ message: 'User successfully created', user: newUser });
+  } catch (error) {
+    res.status(500).json({ message: 'Error creating user', error: error.message });
+  }
+}
+
+module.exports = {
+  createUser
+};
