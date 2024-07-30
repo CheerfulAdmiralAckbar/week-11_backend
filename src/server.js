202098884bd321e79385c6ea5fbeff7530ee9fdd
CheerfulAdmiralAckbar <admiralackbar411@gmail.com>
2024-07-30 11:16:34 +0100

    feature: sync models to database

--- src/server.js
+++ src/server.js
@@ -2,6 +2,9 @@ require('dotenv').config();
 const express = require('express');
 const cors = require('cors');
 
+const User = require('./users/model');
+const Character = require('./characters/model');
+
 const app = express();
 const port = process.env.PORT || 5001;
 
@@ -12,6 +15,12 @@ app.get('/health', (req, res) => {
   res.status(200).json({ message: "API is healthy" });
 })
 
+const syncTables = async () => {
+  await User.sync({ alter: true });
+  await Character.sync({ alter: true });
+}
+
 app.listen(port, () => {
+  syncTables();
   console.log(`Character Vault API listening on ${port}`);
 });
