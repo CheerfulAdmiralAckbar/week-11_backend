9e8e847ad2a2ab48dffb7cc16bde9737d775d9b4
mukhtar mohamed <170721974+donny-LD@users.noreply.github.com>
2024-07-30 14:58:01 +0100

    feature:fished delete code

--- src/users/controllers.js
+++ src/users/controllers.js
@@ -1,32 +1,36 @@
 const User = require("./model");
 
 const createUser = async (req, res) => {
-  console.log('Request Body: ', req.body);
+  console.log("Request Body: ", req.body);
   try {
     const { username, email, password, confirmPassword } = req.body;
 
     if (password !== confirmPassword) {
-      return res.status(401).json({ message: 'Passwords do not match' });
+      return res.status(401).json({ message: "Passwords do not match" });
     }
 
     const newUser = await User.create({
       username: username,
       email: email,
-      password: password
+      password: password,
     });
 
-    res.status(201).json({ message: 'User successfully created', user: newUser });
+    res
+      .status(201)
+      .json({ message: "User successfully created", user: newUser });
   } catch (error) {
-    res.status(500).json({ message: 'Error creating user', error: error.message });
+    res
+      .status(500)
+      .json({ message: "Error creating user", error: error.message });
   }
-}
+};
 
 const updateAccount = async (req, res) => {
   try {
     const filterObj = { id: req.body.id };
     const updateObj = { [req.body.updateKey]: req.body.updateValue };
 
-    await Book.update(updateObj, {
+    await User.update(updateObj, {
       where: filterObj,
     });
   } catch (err) {
@@ -41,17 +45,15 @@ const deleteAccount = async (req, res) => {
     if (!user) {
       return res.status(404).json({ message: "User not found" });
     }
-    await user.destroy();
+    await User.destroy();
     res.status(200).json({ message: "User deleted successfully" });
   } catch (error) {
     res.status(500).json({ message: error.message });
   }
 };
 
-
-
 module.exports = {
   createUser,
   updateAccount,
-  deleteAccount
+  deleteAccount,
 };
--- src/users/routes.js
+++ src/users/routes.js
@@ -4,9 +4,8 @@ const userRouter = Router();
 const { createUser, updateAccount, deleteAccount } = require("./controllers");
 const {} = require("../middleware/auth");
 
-userRouter.post('/signup', createUser);
+userRouter.post("/signup", createUser);
 userRouter.put("/updateAcc", updateAccount);
-userRouter.delete("/deleteAccount",deleteAccount)
-
+userRouter.delete("/deleteAcc", deleteAccount);
 
 module.exports = userRouter;
