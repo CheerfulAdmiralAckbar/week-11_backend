1e4a71969598e3f407fc18b28d0545dd68a0b7a6
LastGreenseer <170721131+LastGreenseer@users.noreply.github.com>
2024-07-30 15:29:09 +0100

    refactor: deleteCharacter route to fit naming convention

--- src/characters/routes.js
+++ src/characters/routes.js
@@ -5,6 +5,6 @@ const { addCharacter, deleteCharacter } = require("./controllers");
 
 charRouter.post("/addCharacter", addCharacter);
 
-charRouter.delete("characters/:name", deleteCharacter)
+charRouter.delete("/Deletecharacter/:name", deleteCharacter)
 
 module.exports = charRouter;
