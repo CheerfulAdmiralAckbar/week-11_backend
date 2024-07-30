453d4573c1ceb2fda0e811a99685a53799ec7f1d
mukhtar mohamed <170721974+donny-LD@users.noreply.github.com>
2024-07-30 15:07:07 +0100

    feature:made delete code

--- src/users/controllers.js
+++ src/users/controllers.js
@@ -63,10 +63,25 @@ const updateAccount = async (req, res) => {
     res.status(501).json({ message: err.message, err: err });
   }
 };
+const deleteAccount = async (req, res) => {
+  try {
+    const { id } = req.params;
+    const user = await User.findByPk(id);
+    if (!user) {
+      return res.status(404).json({ message: "User not found" });
+    }
+    await User.destroy();
+    res.status(200).json({ message: "User deleted successfully" });
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+};
+
 
 module.exports = {
   createUser,
   login,
   updateAccount,
+  deleteAccount,
 };
 
--- src/users/routes.js
+++ src/users/routes.js
@@ -2,11 +2,12 @@ const { Router } = require("express");
 const userRouter = Router();
 
 
-const { createUser, login, updateAccount } = require("./controllers");
+const { createUser, login, updateAccount, deleteAccount } = require("./controllers");
 const {} = require("../middleware/auth");
 
 userRouter.post('/signup', createUser);
 userRouter.post("/login", login);
 userRouter.put("/updateAcc", updateAccount);
+userRouter.delete("/deleteAcc",deleteAccount);
 
 module.exports = userRouter;
