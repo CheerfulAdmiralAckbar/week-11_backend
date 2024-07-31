6bf2e00de9e9b26bedc097aef41b0e8276c01f32
LastGreenseer <170721131+LastGreenseer@users.noreply.github.com>
2024-07-31 10:54:18 +0100

    fix: deleteCharacter route is nor camelCase

--- src/characters/routes.js
+++ src/characters/routes.js
@@ -5,6 +5,6 @@ const { addCharacter, deleteCharacter } = require("./controllers");
 
 charRouter.post("/addCharacter", addCharacter);
 
-charRouter.delete("/deletecharacter/:name", deleteCharacter)
+charRouter.delete("/deleteCharacter/:name", deleteCharacter)
 
 module.exports = charRouter;
